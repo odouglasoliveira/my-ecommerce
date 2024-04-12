587d26e70457ebc9396bc79db6ed7385ee642d29
Douglas Oliveira <doglasolivera13@gmail.com>
2024-04-12 09:43:32 -0300

    error component for not valid routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import ErrorPage from './pages/ErrorPage.jsx';
 import {
   createBrowserRouter,
   RouterProvider,
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
   },
+  {
+    path: "/*",
+    element: <ErrorPage />
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+function ErrorPage() {
+  const navigate = useNavigate();
+  useEffect(() => () => setTimeout(() => navigate("/"), 3000), [navigate])
+
+  return (
+    <>
+      <section className="flex justify-center items-center min-h-full gap-12">
+        <img src="/error-page.svg" alt="Icone de erro" className="w-1/6" />
+        <section className="text-2xl text-center flex flex-col gap-6">
+          <p className="text-3xl font-bold font-sans">Opss...</p>
+          <p className="font-sans">Parece que essa página não existe!</p>
+          <p>Estamos te redirecionando de volta ao nosso app!!</p>
+        </section>
+        <div className="loader absolute bottom-20"></div>
+      </section>
+    </>
+  )
+}
+
+export default ErrorPage
